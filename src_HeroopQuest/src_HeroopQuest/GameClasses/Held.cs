8a2e3fbb020343a7bf0c34ce3d874e3c5ad89c94
sayan <sureshsayan77@gmail.com>
2020-02-21 11:31:17 +0100

    HeldClasse

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace src_HeroopQuest.GameClasses
 {
+    enum HeldType {Barbaar, Dwerg, Elf, Tovenaar}
     class Held
     {
+        public string Naam { get; set; }
+        public string Bechschrijving { get; private set; }
+        public int MaxIntelleigentie { get; private set; }
+        public int MaxLichaam { get; private set; }
+        public int HuidigIntelligentie { get; set; }
+        public int HuidigLichaam { get; set; }
+        public HeldType heldType { get; private set; }
+
+        public int AanvalDobellStenen { get; private set; } = 2;
+        public int VerdigingDobbelStenen { get; set; } = 2;
+        public int LoopDobbelStenen { get; set; } = 2;
     }
 }
